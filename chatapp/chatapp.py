0b2e5e7ccadca573e98c50c97f27e6a6d664b317
Rubi <untalrubi@gmail.com>
2024-11-23 20:39:48 +0100

    feat(CHATAPP): :sparkles: Add input box
    Added an Input box to the chat with a button.

--- chatapp/chatapp.py
+++ chatapp/chatapp.py
@@ -1,17 +1,45 @@
 import reflex as rx
 
 
+def qa(question: str, answer:str) -> rx.Component:
+    return rx.box(
+        rx.box(question, text_align="right"),
+        rx.box(answer, text_align="left"),
+        margin_y="1em",
+    )
+
+
+def chat() -> rx.Component:
+    qa_pairs = [
+        (
+            "What is Reflex?",
+            "A way to build web apps in pure Python!",
+        ),
+        (
+            "What can I make with it?",
+            "Anything from a simple website to a complex web app!",
+        ),
+    ]
+    return rx.box(
+        *[
+            qa(question, answer)
+            for question, answer in qa_pairs
+        ]
+    )
+
+
+def action_bar() -> rx.Component:
+    return rx.hstack(
+        rx.input(placeholder="Ask a question"),
+        rx.button("Ask"),
+    )
+
+
 def index() -> rx.Component:
     return rx.container(
-        rx.box(
-            "What is Reflex?",
-            text_align="right",
-        ),
-        rx.box(
-            "A way to build web apps in pure Python!",
-            text_align="left",
-        ),
-    )
+        chat(),
+        action_bar(),
+        )
 
 
 app = rx.App()
