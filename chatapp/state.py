68b5b5b5fe0000e5b5b27d5d7b0b7b9ae257cef9
Rubi <untalrubi@gmail.com>
2024-11-24 02:08:47 +0100

    feat(STATE): :sparkles: Add asyncio
    Added and used some utilities from the asyncio library

--- chatapp/state.py
+++ chatapp/state.py
@@ -1,5 +1,5 @@
 import reflex as rx
-
+import asyncio
 
 class State(rx.State):
 
@@ -12,3 +12,17 @@ class State(rx.State):
         answer = "I don't know!"
         self.chat_history.append((self.question, answer))
         self.question=""
+
+    async def answer(self):
+        answer = "I don't know!"
+        self.chat_history.append((self.question, ""))
+        self.question=""
+        yield
+
+        for i in range(len(answer)):
+            await asyncio.sleep(0.1)
+            self.chat_history[-1]=(
+                self.chat_history[-1][0],
+                answer[: i+1]
+            )
+            yield
