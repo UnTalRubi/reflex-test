242603b373add805cc73cc53d99c1af5fa3c3f6c
Rubi <untalrubi@gmail.com>
2024-11-24 01:15:16 +0100

    feat(STATE): :sparkles: Add State
    Added the state module to keep track of the variables that can change and the functions to modify them.

---
+++ chatapp/state.py
@@ -0,0 +1,13 @@
+import reflex as rx
+
+
+class State(rx.State):
+
+    question: str
+
+    chat_history: list[tuple[str, str]]
+
+    @rx.event
+    def answer(self):
+        answer = "I don't know!"
+        self.chat_history.append((self.question, answer))
